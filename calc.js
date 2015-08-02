39c81cb556c89d3cb24d1ab4102fe06fac5ad587
bon22801 <bon22801@gmail.com>
2015-08-01 23:55:26 -0400

    set up on click listener

--- calc.js
+++ calc.js
@@ -3,6 +3,33 @@ function multiply(a, b) {return a * b;}
 function divide(a, b) {return a / b;}
 function subtract(a, b) {return a - b;}
 
-console.log(add(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+var input = 0;
+var temp_value = 0;
+var operand;
+    
+$(".nums").on("click", function() {
+  input = input*10 + Number($(this).text());
+  update_result(input);
+});
+
+$(".operands").on("click", function() {
+  //store and reset input
+  temp_value = input;
+  input = 0;
+  
+  operand = $(this).text();
+  update_result(operand);
+});
+
+$(".eval").on("click", function() {
+  console.log("input:" + input);
+  console.log("temp:" + temp_value);
+});
+  
+  
+function update_result(num) {
+  $("#result").text(num);
+}
+
+
+//function evaluate(number1, number2, operand) {}
