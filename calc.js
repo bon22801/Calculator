42751520a1c589d5f66bfd1d5587f253b4a56af1
bon22801 <bon22801@gmail.com>
2015-08-05 01:38:43 -0400

    Made the calculator working! also edited a bit css

--- calc.js
+++ calc.js
@@ -1,10 +1,5 @@
-function add(a, b) {return a + b;}
-function multiply(a, b) {return a * b;}
-function divide(a, b) {return a / b;}
-function subtract(a, b) {return a - b;}
-
 var input = 0;
-var temp_value = 0;
+var previous_input = 0;
 var operand;
     
 $(".nums").on("click", function() {
@@ -14,7 +9,7 @@ $(".nums").on("click", function() {
 
 $(".operands").on("click", function() {
   //store and reset input
-  temp_value = input;
+  previous_input = input;
   input = 0;
   
   operand = $(this).text();
@@ -23,13 +18,48 @@ $(".operands").on("click", function() {
 
 $(".eval").on("click", function() {
   console.log("input:" + input);
-  console.log("temp:" + temp_value);
+  console.log("temp:" + previous_input + "\n");
+  switch(operand) {
+    case "+":
+      input = previous_input + input;
+      update_result(input);    
+      break;
+    case "-":
+      input = previous_input - input;
+      update_result(input);  
+      break;
+    case "x":
+      input = previous_input * input;
+      update_result(input);  
+      break;
+    case "/":
+      input = previous_input / input;
+      update_result(input);  
+      break;  
+    default:
+      console.log("no operand found");
+  }
+});
+
+$(".clear").on("click", function() {
+  reset();
 });
   
-  
+function reset() {
+  previous_input = 0;
+  input = 0;
+  update_result(0);
+}
+
 function update_result(num) {
   $("#result").text(num);
 }
 
+function add(a, b) {return a + b;}
+function multiply(a, b) {return a * b;}
+function divide(a, b) {return a / b;}
+function subtract(a, b) {return a - b;}
 
-//function evaluate(number1, number2, operand) {}
+
+
+
